ffe99ee939474caaed6e17b12c9fbd7fb1efda4e
Srdjan <srkyman@gmail.com>
2019-07-25 20:02:18 +0200

    Animation basics

---
+++ BegineerUnityProject/Assets/_ArtAnimation/Scripts/CicaController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CicaController : MonoBehaviour
+{
+    public Animator cicaAnimator;
+
+    public string attackTrigger;
+    public string walkTrigger;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            cicaAnimator.SetTrigger(attackTrigger);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            cicaAnimator.SetBool(walkTrigger, true);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            cicaAnimator.SetBool(walkTrigger, false);            
+        }
+    }
+}
