620515119a3a0840e7dccc3216126fa043fe4015
Srdjan <srkyman@gmail.com>
2019-08-08 18:14:38 +0200

    Bit refactor and moving trough plevels logic done

---
+++ BegineerUnityProject/Assets/_SideScrollerFigher/Game/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [SerializeField]
+    private GameObject playerPrefab;
+    private Vector3 spawnPosition;
+
+    // Unity msgs
+
+    private void Awake()
+    {
+        instance = this;
+        spawnPosition = GameObject.Find("SpawnPosition").transform.position;        
+    }
+
+    void Start()
+    {
+        // TODO -- here is place to check for player skin, current progress and stuff like that
+        Instantiate(playerPrefab, spawnPosition, Quaternion.identity);
+    }
+
+    // Use this to move trough game
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
---
+++ BegineerUnityProject/Assets/_SideScrollerFigher/Game/Scripts/LevelTransition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTransition : MonoBehaviour
+{
+    /*
+        Expected to have trigger collider on self
+     */
+    
+    [SerializeField]
+    private string goToLevel;
+    [SerializeField]
+    private string triggerWithTag = "Player";
+
+    // TODO -- set layer matrix
+    private void OnTriggerEnter2D(Collider2D other)        
+    {
+        if (other.tag.Equals(triggerWithTag))
+        {
+            Debug.Log("Maybe deactivate player input...");
+            GameManager.instance.LoadLevel(goToLevel);
+        }
+    }
+}
