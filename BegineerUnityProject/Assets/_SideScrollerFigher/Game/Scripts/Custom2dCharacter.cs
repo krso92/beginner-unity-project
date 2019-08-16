7a1ca87d93e25e4a276a1e83ae5588dd80a1bc34
Srdjan <srkyman@gmail.com>
2019-08-16 11:55:45 +0200

    Character dies and health system is working

--- BegineerUnityProject/Assets/_SideScrollerFigher/Game/Scripts/Custom2dCharacter.cs
+++ BegineerUnityProject/Assets/_SideScrollerFigher/Game/Scripts/Custom2dCharacter.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Custom2dCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Animator animator;
+    
+    [SerializeField]
+    private UnityStandardAssets._2D.Platformer2DUserControl controls;
+
+    [SerializeField]
+    private int maxHealth;
+
+    private int health;
+
+    private void Start()
     {
-        
+        health = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        var dmg = other.gameObject.GetComponent<DamageSystem>();
+        if (dmg != null)
+        {
+            health = Mathf.Max(0, health - dmg.DamageAmount);
+        }
+        if (health == 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        animator.SetTrigger("Dead");
+        controls.enabled = false;
+    }
+
+    private void Update()
+    {
+        // A little cheat ;)
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            animator.SetTrigger("Revive");
+            controls.enabled = true;
+        }    
     }
 }
---
+++ BegineerUnityProject/Assets/_SideScrollerFigher/Game/Scripts/DamageSystem.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class DamageSystem : MonoBehaviour
+{
+    [SerializeField]
+    private int damageAmount;
+
+    public int DamageAmount => damageAmount;
+}
