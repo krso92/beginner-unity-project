d1de9d77cbd920a51e9b2dab54df9f6cba49f0d7
Srdjan <srkyman@gmail.com>
2019-12-01 17:23:06 +0100

    Scripta za odlazak nivo ispod, input

---
+++ BegineerUnityProject/Assets/_SideScrollerFigher/Game/Scripts/Gameplay/Levels/PlatformTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTrigger : MonoBehaviour
+{
+    private bool inside;
+
+    [SerializeField]
+    private Collider2D elementCollider;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        inside = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        inside = false;
+        elementCollider.enabled = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("GoLevelDown") && inside)
+        {
+            elementCollider.enabled = false;
+        }
+    }
+}
